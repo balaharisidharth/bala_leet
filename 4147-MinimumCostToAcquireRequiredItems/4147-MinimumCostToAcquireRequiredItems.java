661d863f108d974e4f56502a7e7c06d64912da20
balaharisidharth <balahari2211@gmail.com>
2026-02-25 10:30:43 +0530

    Create 4147-MinimumCostToAcquireRequiredItems

---
+++ 4147-MinimumCostToAcquireRequiredItems/4147-MinimumCostToAcquireRequiredItems.java
@@ -0,0 +1,12 @@
+// Last updated: 25/02/2026, 10:30:42
+class Solution {
+    public long minimumCost(int cost1, int cost2, int costBoth, int need1, int need2) {
+        long min, max, ans;
+        min = Math.min(need1, need2);
+        max = Math.max(need1, need2);
+        ans = (long) need1 * cost1 + (long) need2 * cost2;
+        ans  = Math.min(ans, (long)min * costBoth + (long) (need1 - min) * cost1 + (long) (need2 - min) * cost2);
+        ans = Math.min(ans, (long) max * costBoth);
+        return ans;   
+    }
+}
