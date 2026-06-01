255121ae7a73961cfd7173eb5c28871b31e5b932
balaharisidharth <balahari2211@gmail.com>
2026-06-01 06:41:44 +0530

    File updated at 01/06/2026, 06:41:42

--- 21-MergeTwoSortedLists/21-MergeTwoSortedLists.java
+++ 21-MergeTwoSortedLists/21-MergeTwoSortedLists.java
@@ -1,15 +1,19 @@
-// Last updated: 01/06/2026, 06:41:10
+// Last updated: 01/06/2026, 06:41:42
 1class Solution {
-2    public int removeDuplicates(int[] nums) {
-3        int n = nums.length;
-4        int j=1;
-5        for(int i=1;i<n;i++){
-6            if(nums[i]!=nums[i-1]){
-7                nums[j]=nums[i];
-8                j++;
-9            }
-10        }
-11        return j;
-12        
-13    }
-14}
+2    public int removeElement(int[] nums, int val) {
+3        
+4        int c = 0;
+5        for(int i = 0; i < nums.length; i++){
+6            if(nums[i] != val){
+7                nums[c] = nums[i];
+8                c++;
+9                
+10            }
+11            
+12        }
+13        
+14        return c;
+15    }
+16    
+17
+18}
