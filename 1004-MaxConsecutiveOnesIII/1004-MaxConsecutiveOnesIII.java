4e455d1f263fe35735623859168741f35d6751df
balaharisidharth <balahari2211@gmail.com>
2026-06-03 14:22:32 +0530

    File updated at 03/06/2026, 14:22:32

--- 1004-MaxConsecutiveOnesIII/1004-MaxConsecutiveOnesIII.java
+++ 1004-MaxConsecutiveOnesIII/1004-MaxConsecutiveOnesIII.java
@@ -1,4 +1,4 @@
-// Last updated: 03/06/2026, 14:21:42
+// Last updated: 03/06/2026, 14:22:32
 1class Solution {
 2    public int search(int[] nums, int target) {
 3        int s = 0, e = nums.length - 1;
@@ -7,26 +7,23 @@
 6            int m = (s + e) / 2;
 7            
 8            if (nums[m] == target) return m;
-9            
-10            // Check if left half is sorted
-11            if (nums[s] <= nums[m]) {
-12                if (target >= nums[s] && target < nums[m]) {
-13                    e = m - 1;
-14                } else {
-15                    s = m + 1;
-16                }
-17            } 
-18            // Otherwise right half is sorted
-19            else {
-20                if (target > nums[m] && target <= nums[e]) {
-21                    s = m + 1;
-22                } else {
-23                    e = m - 1;
-24                }
-25            }
-26        }
-27        
-28        return -1;
-29    }
-30}
-31
+9                if (nums[s] <= nums[m]) {
+10                if (target >= nums[s] && target < nums[m]) {
+11                    e = m - 1;
+12                } else {
+13                    s = m + 1;
+14                }
+15            } 
+16            else {
+17                if (target > nums[m] && target <= nums[e]) {
+18                    s = m + 1;
+19                } else {
+20                    e = m - 1;
+21                }
+22            }
+23        }
+24        
+25        return -1;
+26    }
+27}
+28
