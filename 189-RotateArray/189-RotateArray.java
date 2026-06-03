9859362ed7b79cd58c115f68f62fac8a201dbae1
balaharisidharth <balahari2211@gmail.com>
2026-06-03 11:10:50 +0530

    File updated at 03/06/2026, 11:10:49

--- 189-RotateArray/189-RotateArray.java
+++ 189-RotateArray/189-RotateArray.java
@@ -1,4 +1,4 @@
-// Last updated: 03/06/2026, 10:50:24
+// Last updated: 03/06/2026, 11:10:49
 1class Solution {
 2    public void rotate(int[] nums, int k) {
 3       int l=nums.length;
@@ -6,21 +6,23 @@
 5       int m=k;
 6       k%=l;
 7       int j=0;
-8       if(l!=1 &&  m!=0){
-9       while((l-k)<l) {
-10        temp[j]=nums[l-k];
-11        j++;
-12        k--;
-13       }
-14    
-15    for(int i=0;i<(l-k);i++){
-16        if (j<l){
-17        temp[j]=nums[i];
-18        j++;
-19        }
-20    }
-21    for(int i=0;i<l;i++){
-22        nums[i]=temp[i];
-23    }
+8
+9       if(l!=1 &&  m!=0){
+10       while((l-k)<l) {
+11        temp[j]=nums[l-k];
+12        j++;
+13        k--;
+14       }
+15    
+16    for(int i=0;i<(l-k);i++){
+17        if (j<l){
+18        temp[j]=nums[i];
+19        j++;
+20        }
+21    }
+22    for(int i=0;i<l;i++){
+23        nums[i]=temp[i];
 24    }
-25}}
+25    }
+26}}
+27
