53d1b5b18309fd81971a7fd40d2a7718ad26f00d
balaharisidharth <balahari2211@gmail.com>
2026-06-03 12:01:13 +0530

    File updated at 03/06/2026, 12:01:13

--- 3925-ConcatenateArrayWithReverse/3925-ConcatenateArrayWithReverse.java
+++ 3925-ConcatenateArrayWithReverse/3925-ConcatenateArrayWithReverse.java
@@ -1,18 +1,23 @@
-// Last updated: 03/06/2026, 11:45:44
+// Last updated: 03/06/2026, 12:01:13
 1class Solution {
-2    public int[] concatWithReverse(int[] nums) {
-3        int n=nums.length;
-4        int[] arr=new int[2*n];
-5        int i;
-6        for(i=0;i<n;i++){
-7            arr[i]=nums[i];
-8        }
-9        if(i==n){
-10            for(int j=n-1;j>=0;j--){
-11                arr[i]=nums[j];
-12                i++;
-13            }
-14        }
-15        return arr;
-16    }
-17}
+2    public int search(int[] nums, int target) {
+3        int left = 0, right = nums.length - 1;
+4        while(left <= right)
+5        {
+6            int mid = left + (right - left) / 2;
+7            if(nums[mid] == target)
+8            {
+9                return mid;
+10            }
+11            else if(nums[mid] < target)
+12            {
+13                left = mid + 1;
+14            }
+15            else
+16            {
+17                right = mid - 1;
+18            }
+19        }
+20        return -1;
+21    }
+22}
