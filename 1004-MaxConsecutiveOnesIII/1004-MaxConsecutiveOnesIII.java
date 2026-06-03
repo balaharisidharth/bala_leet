f2d7d1c0f26a5fa4f4b16052ec67ce60161dfec9
balaharisidharth <balahari2211@gmail.com>
2026-06-03 16:17:15 +0530

    File updated at 03/06/2026, 16:17:14

--- 1004-MaxConsecutiveOnesIII/1004-MaxConsecutiveOnesIII.java
+++ 1004-MaxConsecutiveOnesIII/1004-MaxConsecutiveOnesIII.java
@@ -1,4 +1,4 @@
-// Last updated: 03/06/2026, 16:10:00
+// Last updated: 03/06/2026, 16:17:14
 1class Solution {
 2    public int calculate(int[] piles,int mid){
 3        int c=0;
@@ -10,41 +10,36 @@
 9        }
 10        return c;
 11    }
-12
-13
-14
-15    public int minEatingSpeed(int[] piles, int h) {
-16        int max=1;
-17        int sum=0;
-18        int l=piles.length;
-19        int right=1;
-20        for(int ele : piles){
-21            sum+=ele;
-22            if(max<=ele){
-23                max=ele;
-24            }
-25        }
-26        int left = max;
-27        if(l==h){
-28            return max;
-29        }
-30        else if(sum==h){
-31            return 1;
-32        }
-33        else{
-34            while( right < left){
-35            int mid=(right+left)/2;
-36            int hrs=calculate(piles,mid);
-37            if(hrs>h){
-38                right=mid+1;
+12    public int minEatingSpeed(int[] piles, int h) {
+13        int max=1;
+14        int sum=0;
+15        int l=piles.length;
+16        int right=1;
+17        for(int ele : piles){
+18            sum+=ele;
+19            if(max<=ele){
+20                max=ele;
+21            }
+22        }
+23        int left = max;
+24        if(l==h){
+25            return max;
+26        }
+27        // else if(sum==h){
+28        //     return 1;
+29        // }
+30        else{
+31            while( right < left){
+32            int mid=(right+left)/2;
+33            int hrs=calculate(piles,mid);
+34            if(hrs>h){
+35                right=mid+1;
+36            }
+37            else{
+38                left=mid;
 39            }
-40            else{
-41                left=mid;
-42            }
-43            }
-44
-45
-46        }
-47        return left;
-48    }
-49}
+40            }
+41        }
+42        return left;
+43    }
+44}
