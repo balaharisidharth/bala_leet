a02f841ab83a29696629df2fe97e5767ed0bc067
balaharisidharth <balahari2211@gmail.com>
2026-06-12 13:33:10 +0530

    File updated at 12/06/2026, 13:33:08

--- 46-Permutations/46-Permutations.java
+++ 46-Permutations/46-Permutations.java
@@ -1,27 +1,26 @@
-// Last updated: 12/06/2026, 13:32:52
+// Last updated: 12/06/2026, 13:33:08
 1class Solution {
 2    public List<List<Integer>> permute(int[] nums) {
 3        List<List<Integer>> ans = new ArrayList<>();
 4        List<Integer> temp = new ArrayList<>();
-5        List<Integer> curr = new ArrayList<>();
-6        method(ans, temp, nums);
-7        return ans;
-8    }
-9
-10    void method(List<List<Integer>> ans, List<Integer> temp, int[] nums) {
-11        int n = nums.length;
-12        if (temp.size() == n) {
-13            ans.add(new ArrayList<>(temp));
-14        } else {
-15
-16            for (int i = 0; i < n; i++) {
-17                if (temp.contains(nums[i])) {
-18                    continue;
-19                }
-20                temp.add(nums[i]);
-21                method(ans, temp, nums);
-22                temp.remove(temp.size() - 1);
-23            }
-24        }
-25    }
-26}
+5        method(ans, temp, nums);
+6        return ans;
+7    }
+8
+9    void method(List<List<Integer>> ans, List<Integer> temp, int[] nums) {
+10        int n = nums.length;
+11        if (temp.size() == n) {
+12            ans.add(new ArrayList<>(temp));
+13        } else {
+14
+15            for (int i = 0; i < n; i++) {
+16                if (temp.contains(nums[i])) {
+17                    continue;
+18                }
+19                temp.add(nums[i]);
+20                method(ans, temp, nums);
+21                temp.remove(temp.size() - 1);
+22            }
+23        }
+24    }
+25}
