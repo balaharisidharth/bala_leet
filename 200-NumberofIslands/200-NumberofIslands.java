24d7586eef0dfcf3c13d8370807a01af07fa900f
balaharisidharth <balahari2211@gmail.com>
2026-06-13 16:20:28 +0530

    File updated at 13/06/2026, 16:20:28

--- 200-NumberofIslands/200-NumberofIslands.java
+++ 200-NumberofIslands/200-NumberofIslands.java
@@ -1,28 +1,45 @@
-// Last updated: 13/06/2026, 14:57:44
-1class Solution {
-2    public int numIslands(char[][] grid) {
-3         int c=0;
-4         int row=grid.length;
-5         int col=grid[0].length;
-6         for(int i=0;i<row;i++){
-7            for(int j=0;j<col;j++){
-8                if(grid[i][j]=='1'){
-9
-10                c++;
-11                travel(grid,i,j);
-12                }
-13            }
-14         }
-15         return c;
-16    }
-17    void travel(char[][] grid,int row,int col){
-18        if(col>=grid[0].length || col<0 || row<0 || row>=grid.length || grid[row][col]=='0'){
-19            return;
-20        }
-21        grid[row][col]='0';
-22        travel(grid,row+1,col);
-23        travel(grid,row,col-1);
-24        travel(grid,row-1,col);
-25        travel(grid,row,col+1);
-26    }
-27}
+// Last updated: 13/06/2026, 16:20:28
+1/*
+2// Definition for a Node.
+3class Node {
+4    public int val;
+5    public List<Node> neighbors;
+6    public Node() {
+7        val = 0;
+8        neighbors = new ArrayList<Node>();
+9    }
+10    public Node(int _val) {
+11        val = _val;
+12        neighbors = new ArrayList<Node>();
+13    }
+14    public Node(int _val, ArrayList<Node> _neighbors) {
+15        val = _val;
+16        neighbors = _neighbors;
+17    }
+18}
+19*/
+20
+21class Solution {
+22    private Map<Node, Node> clonedNodes = new HashMap<>();
+23
+24    public Node cloneGraph(Node node) {
+25        if (node == null)
+26            return null;
+27
+28        // If the node has already been cloned, return the cloned node
+29        if (clonedNodes.containsKey(node)) {
+30            return clonedNodes.get(node);
+31        }
+32
+33        // Clone the node (without neighbors initially)
+34        Node clonedNode = new Node(node.val);
+35        clonedNodes.put(node, clonedNode);
+36
+37        // Clone the neighbors recursively
+38        for (Node neighbor : node.neighbors) {
+39            clonedNode.neighbors.add(cloneGraph(neighbor));
+40        }
+41
+42        return clonedNode;
+43    }
+44}
