daa2b6d2458540f24ac784a26796fbd947a20001
balaharisidharth <balahari2211@gmail.com>
2026-06-27 09:28:55 +0530

    File updated at 27/06/2026, 09:28:55

--- 10-RegularExpressionMatching/10-RegularExpressionMatching.java
+++ 10-RegularExpressionMatching/10-RegularExpressionMatching.java
@@ -1,4 +1,4 @@
-// Last updated: 27/06/2026, 09:28:25
+// Last updated: 27/06/2026, 09:28:55
 1/**
 2 * Definition for singly-linked list.
 3 * public class ListNode {
@@ -10,31 +10,35 @@
 9 * }
 10 */
 11class Solution {
-12    public ListNode mergeKLists(ListNode[] lists) {
-13     
-14        //coz node is not comparable in java unlike Integer is we used custom comparator
-15       PriorityQueue<ListNode> pq = new  PriorityQueue<>((a,b)-> Integer.compare(a.val,b.val));
-16
-17        //get all the node and add it in pq
-18       for(ListNode node : lists){
-19         if(node != null){
-20            pq.offer(node);
-21         }
-22       }
-23
-24       ListNode dummy = new ListNode(0);
-25       ListNode temp = dummy;
-26       
-27       //we are using minHeap so it will give me smallest node on pop
-28       while(!pq.isEmpty()){
-29            ListNode smallest = pq.poll(); //get the smallest
-30            temp.next = smallest; //adding in ans node
-31            temp = temp.next; //move pointer for adding next smaller node
-32
-33            //also check if next node occurs in that specific list or not from where we are extracting
-34            if(smallest.next != null) pq.offer(smallest.next);
-35       }
-36    return dummy.next;
-37
-38    }
-39}
+12    ListNode head,tail;
+13    public ListNode reverseKGroup(ListNode givenHead, int k){
+14        Stack<ListNode> stk = new Stack<>();
+15        ListNode node = givenHead;
+16        head = tail = null;
+17        while(node != null){
+18            stk.push(node);
+19            node = node.next;
+20            if(stk.size() == k){
+21                insert(stk);
+22                tail.next = node;
+23            }
+24        }
+25       
+26        return head;
+27        
+28    }
+29    private void insert(Stack<ListNode> stk){
+30        while(!stk.isEmpty()){
+31            ListNode node = stk.pop();
+32            node.next = null;
+33            if(head == null){
+34                tail = head = node;
+35            }
+36            else{
+37                tail.next = node;
+38                tail = node;
+39            }
+40        }
+41    }
+42
+43}
