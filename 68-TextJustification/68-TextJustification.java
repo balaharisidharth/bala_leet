be13732a70f22b373bb649266657f95345a7a7c9
balaharisidharth <balahari2211@gmail.com>
2026-07-27 09:03:40 +0530

    File updated at 27/07/2026, 09:03:39

--- 68-TextJustification/68-TextJustification.java
+++ 68-TextJustification/68-TextJustification.java
@@ -1,43 +1,22 @@
-// Last updated: 27/07/2026, 09:02:43
+// Last updated: 27/07/2026, 09:03:39
 1public class Solution {
-2public int maximalRectangle(char[][] matrix) {
-3    if(matrix == null || matrix.length == 0 || matrix[0].length == 0) return 0;
-4    
-5    int[] height = new int[matrix[0].length];
-6    for(int i = 0; i < matrix[0].length; i ++){
-7        if(matrix[0][i] == '1') height[i] = 1;
-8    }
-9    int result = largestInLine(height);
-10    for(int i = 1; i < matrix.length; i ++){
-11        resetHeight(matrix, height, i);
-12        result = Math.max(result, largestInLine(height));
-13    }
-14    
-15    return result;
-16}
-17
-18private void resetHeight(char[][] matrix, int[] height, int idx){
-19    for(int i = 0; i < matrix[0].length; i ++){
-20        if(matrix[idx][i] == '1') height[i] += 1;
-21        else height[i] = 0;
-22    }
-23}    
-24
-25public int largestInLine(int[] height) {
-26    if(height == null || height.length == 0) return 0;
-27    int len = height.length;
-28    Stack<Integer> s = new Stack<Integer>();
-29    int maxArea = 0;
-30    for(int i = 0; i <= len; i++){
-31        int h = (i == len ? 0 : height[i]);
-32        if(s.isEmpty() || h >= height[s.peek()]){
-33            s.push(i);
-34        }else{
-35            int tp = s.pop();
-36            maxArea = Math.max(maxArea, height[tp] * (s.isEmpty() ? i : i - 1 - s.peek()));
-37            i--;
-38        }
-39    }
-40    return maxArea;
-41}
-42}
+2    int max = Integer.MIN_VALUE;
+3    
+4    public int maxPathSum(TreeNode root) {
+5        helper(root);
+6        return max;
+7    }
+8    
+9    // helper returns the max branch 
+10    // plus current node's value
+11    int helper(TreeNode root) {
+12        if (root == null) return 0;
+13        
+14        int left = Math.max(helper(root.left), 0);
+15        int right = Math.max(helper(root.right), 0);
+16        
+17        max = Math.max(max, root.val + left + right);
+18        
+19        return root.val + Math.max(left, right);
+20    }
+21}
