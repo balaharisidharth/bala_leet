8b2febe4123fd7ffcf53407b3c40a946906aad1e
balaharisidharth <balahari2211@gmail.com>
2026-08-03 09:00:48 +0530

    File updated at 03/08/2026, 09:00:48

--- 39-CombinationSum/39-CombinationSum.java
+++ 39-CombinationSum/39-CombinationSum.java
@@ -1,24 +1,30 @@
-// Last updated: 03/08/2026, 08:58:19
-1public class Solution {
-2    public String multiply(String num1, String num2) {
-3        int n1 = num1.length(), n2 = num2.length();
-4        int[] products = new int[n1 + n2];
-5        for (int i = n1 - 1; i >= 0; i--) {
-6            for (int j = n2 - 1; j >= 0; j--) {
-7                int d1 = num1.charAt(i) - '0';
-8                int d2 = num2.charAt(j) - '0';
-9                products[i + j + 1] += d1 * d2;
-10            }
-11        }
-12        int carry = 0;
-13        for (int i = products.length - 1; i >= 0; i--) {
-14            int tmp = (products[i] + carry) % 10;
-15            carry = (products[i] + carry) / 10;
-16            products[i] = tmp;
-17        }
-18        StringBuilder sb = new StringBuilder();
-19        for (int num : products) sb.append(num);
-20        while (sb.length() != 0 && sb.charAt(0) == '0') sb.deleteCharAt(0);
-21        return sb.length() == 0 ? "0" : sb.toString();
-22    }
-23}
+// Last updated: 03/08/2026, 09:00:48
+1class Solution {
+2    public int jump(int[] nums) {
+3        int ans = 0;   // number of minimum jumps taken
+4        int end = 0;   // end of the current jump range 
+5        int farthest = 0;  // farthest index we can reach from current level
+6
+7        // we stop at nums.length - 1 beacase once we reach or pass it, we're done
+8
+9        for(int i = 0; i < nums.length - 1; ++i){
+10            // update the farthest reachable index from the current position
+11            farthest = Math.max(farthest, i + nums[i]);
+12
+13            // If we can already reach or pass the last index,
+14            // take one more jump and finish
+15            if(farthest >= nums.length - 1){
+16                ++ans;
+17                break;
+18            }
+19
+20            // when we've iterated through the current "level" (jump range),
+21            // it's time to make next jump
+22            if( i == end ){
+23                ++ans; // increase the jump count
+24                end = farthest; // update the boundry for the next level
+25            }             
+26        }
+27        return ans;        
+28    }
+29}
