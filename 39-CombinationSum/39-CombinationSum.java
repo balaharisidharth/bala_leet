13ec3ac9d2d8c24054a07adea614b88fbb19651e
balaharisidharth <balahari2211@gmail.com>
2026-08-03 09:05:12 +0530

    File updated at 03/08/2026, 09:05:12

--- 39-CombinationSum/39-CombinationSum.java
+++ 39-CombinationSum/39-CombinationSum.java
@@ -1,57 +1,43 @@
-// Last updated: 03/08/2026, 09:03:56
+// Last updated: 03/08/2026, 09:05:12
 1class Solution {
-2    int count = 0;
-3    public int totalNQueens(int n) {
-4       
-5        char[][] board = new char[n][n];
-6
-7        for(int i = 0; i < n; i++){
-8            for(int j = 0; j < n; j++){
-9                board[i][j] = '.';
-10            }
-11        }
-12        nqueen(board, 0, n);
-13        return count;
-14    }
-15    private void nqueen(char[][] board, int row, int n){
+2    public List<Integer> spiralOrder(int[][] matrix) {
+3        List<Integer> ans = new ArrayList<>();          // Stores the Output
+4
+5        int top = 0;                                    // Top row
+6        int rightColumn = matrix[0].length - 1;     // Right Column
+7        int bottom = matrix.length - 1;             // Bottom row
+8        int leftColumn = 0;                         // Left Column
+9
+10        while (top <= bottom && rightColumn >= leftColumn) {
+11            // Print Top
+12            for (int i = leftColumn; i <= rightColumn ; i++) {
+13                ans.add(matrix[top][i]);
+14            }
+15            top++;
 16
-17        // Base case
-18        if(row == n){
-19            count++;
-20            return;
-21        }
-22        for(int col = 0; col < n; col++){
-23            if(isSafe(board,row,col,n)){
-24                board[row][col] = 'Q';
-25
-26                nqueen(board, row+1, n);
-27
-28                board[row][col] = '.';
+17            // Print Right Column
+18            for (int i = top; i <= bottom; i++) {
+19                ans.add(matrix[i][rightColumn]);
+20            }
+21            rightColumn--;
+22
+23            // Print bottom
+24            if(top <= bottom){
+25                for (int i = rightColumn; i >= leftColumn; i--) {
+26                    ans.add(matrix[bottom][i]);
+27                }
+28                bottom--;
 29            }
-30        }
-31    }
-32    private boolean isSafe(char[][] board, int row ,int col, int n){
-33        int i,j;
-34        // chech column
-35        for( i = 0; i < n; i++){
-36           if(board[i][col] == 'Q') return false;
-37        }
-38        // check /
-39         i = row;
-40         j = col;
-41        while(i >= 0 && j < n){
-42            if(board[i][j] == 'Q') return false;
-43            i--;
-44            j++;
-45        }
-46        // check \
-47        i = row;
-48        j = col;
-49        while(i >= 0 && j >= 0){
-50            if(board[i][j] == 'Q') return false;
-51            i--;
-52            j--; 
-53    }
-54    return true;
-55    }
-56}
+30
+31            // Print Left Column
+32            if(leftColumn <= rightColumn){
+33                for (int i = bottom; i >= top; i--) {
+34                    ans.add(matrix[i][leftColumn]);
+35                }
+36                leftColumn++;
+37            }
+38            
+39        }
+40        return ans;
+41    }
+42}
