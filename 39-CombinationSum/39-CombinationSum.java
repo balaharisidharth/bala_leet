b5bf0ab1062952bce71acc89739e0f4d58206e1c
balaharisidharth <balahari2211@gmail.com>
2026-08-03 08:57:25 +0530

    File updated at 03/08/2026, 08:57:25

--- 39-CombinationSum/39-CombinationSum.java
+++ 39-CombinationSum/39-CombinationSum.java
@@ -1,25 +1,12 @@
-// Last updated: 03/08/2026, 08:56:47
+// Last updated: 03/08/2026, 08:57:25
 1class Solution {
-2    public List<List<Integer>> combinationSum(int[] candidates, int target) {
-3        List<List<Integer>> res = new ArrayList<>();
-4
-5        makeCombination(candidates, target, 0, new ArrayList<>(), 0, res);
-6        return res;        
-7    }
-8
-9    private void makeCombination(int[] candidates, int target, int idx, List<Integer> comb, int total, List<List<Integer>> res) {
-10        if (total == target) {
-11            res.add(new ArrayList<>(comb));
-12            return;
-13        }
-14
-15        if (total > target || idx >= candidates.length) {
-16            return;
-17        }
-18
-19        comb.add(candidates[idx]);
-20        makeCombination(candidates, target, idx, comb, total + candidates[idx], res);
-21        comb.remove(comb.size() - 1);
-22        makeCombination(candidates, target, idx + 1, comb, total, res);
-23    }    
-24}
+2    public int firstMissingPositive(int[] nums) {
+3        Arrays.sort(nums);
+4        int count=1;
+5        for(int i=0;i<nums.length;i++){
+6         if(count==nums[i]) count++;
+7         else if(nums[i]>count) return count;
+8        }
+9        return count;
+10    }
+11}
