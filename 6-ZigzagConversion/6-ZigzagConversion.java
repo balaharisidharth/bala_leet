7d46df59d568c43138358e4042ca2ce0bfb35cf0
balaharisidharth <balahari2211@gmail.com>
2026-08-03 09:20:24 +0530

    File updated at 03/08/2026, 09:20:24

--- 6-ZigzagConversion/6-ZigzagConversion.java
+++ 6-ZigzagConversion/6-ZigzagConversion.java
@@ -1,28 +1,64 @@
-// Last updated: 03/08/2026, 09:19:14
+// Last updated: 03/08/2026, 09:20:24
 1class Solution {
-2   public  String countAndSay(int n) {
-3        String[] weKnow = {"1","11","21","1211","111221","312211","13112221","1113213211","31131211131221"};
-4        return findRle(n,weKnow);
-5    }
-6
-7
-8    public  String findRle(int n,String[] arr ){
-9        if (n <= 9){
-10            return arr[n-1];
-11        }
-12        String str =  findRle(n-1,arr);
-13        StringBuilder ans = new StringBuilder();
-14        int count = 1;
-15        for (int i = 1; i <= str.length(); i++) {
-16            if (i < str.length() &&str.charAt(i-1) == str.charAt(i)){
-17                count++;
-18            }else{
-19                ans.append(count);
-20                ans.append(str.charAt(i-1));
-21                count = 1;
-22            }
-23        }
-24        return ans.toString();
-25
-26    }
-27}
+2    public int[][] generateMatrix(int n) {
+3        // Result matrix
+4        int[][] spiralMatrix = new int[n][n];
+5
+6        // Directions: right, down, left, up
+7        int[][] directions = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+8
+9        // Current direction index
+10        int index = 0;
+11
+12        // Number to be filled in the matrix
+13        int num = 1;
+14
+15        // Boundaries of the current spiral layer
+16        int maxRows = n - 1;
+17        int minRows = 0;
+18        int maxCols = n - 1;
+19        int minCols = 0;
+20
+21        // Start from top-left corner
+22        for (int row = 0, col = 0;
+23             row >= minRows && col >= minCols && row <= maxRows && col <= maxCols; ) {
+24
+25            // Fill current cell
+26            spiralMatrix[row][col] = num++;
+27
+28            // If moving right and reached right boundary,
+29            // turn down and shrink top boundary
+30            if (index == 0 && col == maxCols) {
+31                index++;
+32                minRows++;
+33            }
+34
+35            // If moving down and reached bottom boundary,
+36            // turn left and shrink right boundary
+37            if (index == 1 && row == maxRows) {
+38                index++;
+39                maxCols--;
+40            }
+41
+42            // If moving left and reached left boundary,
+43            // turn up and shrink bottom boundary
+44            if (index == 2 && col == minCols) {
+45                index++;
+46                maxRows--;
+47            }
+48
+49            // If moving up and reached top boundary,
+50            // turn right and shrink left boundary
+51            if (index == 3 && row == minRows) {
+52                index = 0;
+53                minCols++;
+54            }
+55
+56            // Move to next cell based on current direction
+57            row += directions[index][0];
+58            col += directions[index][1];
+59        }
+60
+61        return spiralMatrix;
+62    }
+63}
