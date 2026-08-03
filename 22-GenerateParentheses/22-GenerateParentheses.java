dd48f39bb088aec1d99477049e83728d8b47f664
balaharisidharth <balahari2211@gmail.com>
2026-08-03 09:08:27 +0530

    File updated at 03/08/2026, 09:08:27

--- 22-GenerateParentheses/22-GenerateParentheses.java
+++ 22-GenerateParentheses/22-GenerateParentheses.java
@@ -1,24 +1,49 @@
-// Last updated: 03/08/2026, 09:07:33
+// Last updated: 03/08/2026, 09:08:27
 1class Solution {
-2  public int minDistance(String word1, String word2) {
-3    final int m = word1.length();//first word length
-4    final int n = word2.length();///second word length
-5    // dp[i][j] := min # of operations to convert word1[0..i) to word2[0..j)
-6    int[][] dp = new int[m + 1][n + 1];
-7
-8    for (int i = 1; i <= m; ++i)
-9      dp[i][0] = i;
+2    private int rows;
+3    private int cols;
+4    private Set<String> visited;
+5
+6    public boolean exist(char[][] board, String word) {
+7        rows = board.length;
+8        cols = board[0].length;
+9        visited = new HashSet<>();
 10
-11    for (int j = 1; j <= n; ++j)
-12      dp[0][j] = j;
-13
-14    for (int i = 1; i <= m; ++i)
-15      for (int j = 1; j <= n; ++j)
-16        if (word1.charAt(i - 1) == word2.charAt(j - 1))//same characters
-17          dp[i][j] = dp[i - 1][j - 1];//no operation
-18        else
-19          dp[i][j] = Math.min(dp[i - 1][j - 1], Math.min(dp[i - 1][j], dp[i][j - 1])) + 1;                      //replace               //delete        //insert
-20
-21    return dp[m][n];
-22  }
-23}
+11        Map<Character, Integer> count = new HashMap<>();
+12        for (char c : word.toCharArray()) {
+13            count.put(c, count.getOrDefault(c, 0) + 1);
+14        }
+15
+16        if (count.getOrDefault(word.charAt(0), 0) > count.getOrDefault(word.charAt(word.length() - 1), 0)) {
+17            word = new StringBuilder(word).reverse().toString();
+18        }
+19
+20        for (int r = 0; r < rows; r++) {
+21            for (int c = 0; c < cols; c++) {
+22                if (dfs(board, word, r, c, 0)) {
+23                    return true;
+24                }
+25            }
+26        }
+27
+28        return false;
+29    }
+30
+31    private boolean dfs(char[][] board, String word, int r, int c, int k) {
+32        if (k == word.length()) {
+33            return true;
+34        }
+35
+36        if (r < 0 || r >= rows || c < 0 || c >= cols || visited.contains(r + "," + c) || board[r][c] != word.charAt(k)) {
+37            return false;
+38        }
+39
+40        visited.add(r + "," + c);
+41        boolean res = dfs(board, word, r + 1, c, k + 1) ||
+42                      dfs(board, word, r - 1, c, k + 1) ||
+43                      dfs(board, word, r, c + 1, k + 1) ||
+44                      dfs(board, word, r, c - 1, k + 1);
+45        visited.remove(r + "," + c);
+46        return res;
+47    }    
+48}
