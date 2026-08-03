403f7b2f67dae03f6d70ae02a70c03101faf9eec
balaharisidharth <balahari2211@gmail.com>
2026-08-03 09:23:36 +0530

    File updated at 03/08/2026, 09:23:36

--- 6-ZigzagConversion/6-ZigzagConversion.java
+++ 6-ZigzagConversion/6-ZigzagConversion.java
@@ -1,41 +1,33 @@
-// Last updated: 03/08/2026, 09:21:06
+// Last updated: 03/08/2026, 09:23:36
 1class Solution {
-2
-3    private int solve(int i, int j, int m, int n, int[][] dp) {
-4
-5        if (i >= m || j >= n) {
-6            return 0;
-7        }
-8
-9        if (i == m - 1 || j == n - 1) {
-10            return 1;
-11        }
-12
-13        if (dp[i][j] != -1) {
-14            return dp[i][j];
-15        }
-16
-17        int right = solve(i, j + 1, m, n, dp);
-18        int down = solve(i + 1, j, m, n, dp);
-19
-20        return dp[i][j] = right + down;
-21    }
-22
-23    public int uniquePaths(int m, int n) {
-24
-25        if (m == 1 && n == 1) {
-26            return 1;
-27        }
-28
-29        int[][] dp = new int[m][n];
-30
-31        for (int[] row : dp) {
-32            Arrays.fill(row, -1);
-33        }
-34
-35        int right = solve(0, 1, m, n, dp);
-36        int down = solve(1, 0, m, n, dp);
-37
-38        return right + down;
-39    }
-40}
+2    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+3        int m=obstacleGrid.length;
+4        int n=obstacleGrid[0].length;
+5        int[][] dp=new int[m][n];
+6        if(obstacleGrid[0][0]==1||obstacleGrid[m-1][n-1]==1){
+7            return 0;
+8        }
+9        dp[0][0]=1;
+10        for(int i=1;i<m;i++){
+11            if(obstacleGrid[i][0]==0){
+12                dp[i][0]=dp[i-1][0];
+13            }
+14        }
+15        for(int j=1;j<n;j++){
+16            if(obstacleGrid[0][j]==0){
+17                dp[0][j]=dp[0][j-1];
+18            }
+19        }
+20        for(int i=1;i<m;i++){
+21            for(int j=1;j<n;j++){
+22                if(obstacleGrid[i][j]==1){
+23                    dp[i][j]=0;
+24                }
+25                else{
+26                    dp[i][j]=dp[i-1][j]+dp[i][j-1];
+27                }
+28            }
+29        }
+30        return dp[m-1][n-1];
+31    }
+32}
