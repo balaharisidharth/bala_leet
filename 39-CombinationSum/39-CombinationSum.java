7ef17a4e97c37d72b210d946748fa0c1f2fda583
balaharisidharth <balahari2211@gmail.com>
2026-08-03 08:58:19 +0530

    File updated at 03/08/2026, 08:58:19

--- 39-CombinationSum/39-CombinationSum.java
+++ 39-CombinationSum/39-CombinationSum.java
@@ -1,12 +1,24 @@
-// Last updated: 03/08/2026, 08:57:25
-1class Solution {
-2    public int firstMissingPositive(int[] nums) {
-3        Arrays.sort(nums);
-4        int count=1;
-5        for(int i=0;i<nums.length;i++){
-6         if(count==nums[i]) count++;
-7         else if(nums[i]>count) return count;
-8        }
-9        return count;
-10    }
-11}
+// Last updated: 03/08/2026, 08:58:19
+1public class Solution {
+2    public String multiply(String num1, String num2) {
+3        int n1 = num1.length(), n2 = num2.length();
+4        int[] products = new int[n1 + n2];
+5        for (int i = n1 - 1; i >= 0; i--) {
+6            for (int j = n2 - 1; j >= 0; j--) {
+7                int d1 = num1.charAt(i) - '0';
+8                int d2 = num2.charAt(j) - '0';
+9                products[i + j + 1] += d1 * d2;
+10            }
+11        }
+12        int carry = 0;
+13        for (int i = products.length - 1; i >= 0; i--) {
+14            int tmp = (products[i] + carry) % 10;
+15            carry = (products[i] + carry) / 10;
+16            products[i] = tmp;
+17        }
+18        StringBuilder sb = new StringBuilder();
+19        for (int num : products) sb.append(num);
+20        while (sb.length() != 0 && sb.charAt(0) == '0') sb.deleteCharAt(0);
+21        return sb.length() == 0 ? "0" : sb.toString();
+22    }
+23}
