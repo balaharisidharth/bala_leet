ccc5de1b0f01dac3f3fff1cb282e33b86efc4837
balaharisidharth <balahari2211@gmail.com>
2026-08-03 08:54:23 +0530

    File updated at 03/08/2026, 08:54:24

--- 17-LetterCombinationsofaPhoneNumber/17-LetterCombinationsofaPhoneNumber.java
+++ 17-LetterCombinationsofaPhoneNumber/17-LetterCombinationsofaPhoneNumber.java
@@ -1,4 +1,4 @@
-// Last updated: 03/08/2026, 08:52:31
+// Last updated: 03/08/2026, 08:54:24
 1/**
 2 * Definition for singly-linked list.
 3 * public class ListNode {
@@ -6,36 +6,33 @@
 5 *     ListNode next;
 6 *     ListNode() {}
 7 *     ListNode(int val) { this.val = val; }
-8 *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
-9 * }
-10 */
-11class Solution {
-12    public ListNode removeNthFromEnd(ListNode head, int n) {
-13
-14        ListNode dummy = new ListNode(0);
-15
-16        ListNode temp = head;
+8 *     ListNode(int val, ListNode next) {
+9 *         this.val = val;
+10 *         this.next = next;
+11 *     }
+12 * }
+13 */
+14
+15class Solution {
+16    public ListNode swapPairs(ListNode head) {
 17
-18        int size = 0;
-19
-20        while(temp != null){
-21            temp = temp.next;
-22            size++;
-23        }
+18        ListNode dummy = new ListNode(0);
+19        dummy.next = head;
+20
+21        ListNode prev = dummy;
+22
+23        while (prev.next != null && prev.next.next != null) {
 24
-25        temp = dummy;
-26
-27        n = size - n + 1;
-28
-29        for(int i = 1; i < n; i++){
-30
-31            temp.next = head;
-32            temp = temp.next;
-33            head = head.next;
-34        }
-35
-36        temp.next = head.next;
-37
-38        return dummy.next;
-39    }
-40}
+25            ListNode first = prev.next;
+26            ListNode second = first.next;
+27
+28            first.next = second.next;
+29            second.next = first;
+30            prev.next = second;
+31
+32            prev = first;
+33        }
+34
+35        return dummy.next;
+36    }
+37}
