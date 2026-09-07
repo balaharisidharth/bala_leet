83e67254111791d6c62c367c37f2649ef24b2b6e
balaharisidharth <balahari2211@gmail.com>
2026-09-07 09:41:29 +0530

    File updated at 07/09/2026, 09:41:28

--- 65-ValidNumber/65-ValidNumber.java
+++ 65-ValidNumber/65-ValidNumber.java
@@ -1,24 +1,11 @@
-// Last updated: 07/09/2026, 09:10:47
+// Last updated: 07/09/2026, 09:41:28
 1class Solution {
-2    public boolean isNumber(String s) {
-3        boolean isdot = false, ise = false, nums = false;
-4        for (int i = 0; i < s.length(); i++) {
-5            char c = s.charAt(i);
-6            if (Character.isDigit(c)) nums = true;
-7            else if (c == '+' || c == '-') {
-8                if (i > 0 && s.charAt(i - 1) != 'e' && s.charAt(i - 1) != 'E') return false;
-9            } 
-10            else if (c == 'e' || c == 'E') {
-11                if (ise || !nums) return false;
-12                ise = true;
-13                nums = false;
-14            } 
-15            else if (c == '.') {
-16                if (isdot || ise) return false;
-17                isdot = true;
-18            } 
-19            else return false;
-20        }
-21        return nums;
-22    }
-23}
+2    public boolean canJump(int[] nums) {
+3       int reachable = 0;
+4       for(int i = 0; i < nums.length; i ++) {
+5           if(i > reachable) return false;
+6           reachable = Math.max(reachable, i + nums[i]);
+7       } 
+8       return true;
+9    }
+10}
